8a398a0d1c67e1bf8df38d182d95be740fc0588c
Aki Miyazaki <akimisaki3@gmail.com>
2018-12-13 15:05:04 +0900

    works

--- WebContent/examples/bone/rotatelock/js/Example.js
+++ WebContent/examples/bone/rotatelock/js/Example.js
@@ -15,11 +15,12 @@ Example=function(application){
 	
 	this.container=null;//add mesh here
 	var boneList;
-	
+	var lastEuler=new THREE.Euler();
 	ap.signals.transformChanged.add(function(){
-		console.log(ap.transformControls.axis);//null,X,Y,Z
+		//console.log(ap.transformControls.axis);//null,X,Y,Z
 	});
 
+	
 	
 	AppUtils.loadMesh(url,function(mesh){
 		console.log("loadGltfMesh:",url);
@@ -71,33 +72,113 @@ Example=function(application){
 		
 		this.container.add(scope.boneAttachControler.object3d);
 		
-		
+		//var lastQ=new THREE.Quaternion();
+		var rotationControls={};
 		//link to bone
 		var index=0;
+		
+
+		
 		boneList.forEach(function(bone){
 			if(!Mbl3dUtils.isFingerBoneName(bone.name) && !Mbl3dUtils.isTwistBoneName(bone.name)){
 				//console.log("false",bone.name);
 				//THREE.SphereGeometry(2)
 				var sphere=new THREE.Mesh(new THREE.SphereGeometry(2),new THREE.MeshBasicMaterial({color:0x880000,depthTest:false,transparent:true,opacity:.5}));
+				rotationControls[bone.name]=sphere;
 				scope.boneAttachControler.containerList[index].add(sphere);
 				sphere.boneIndex=index;
 				sphere.quaternion.copy(boneList[index].quaternion);
 				var cbone=index;
-				var enableFire=true;
+				
+				var q=new THREE.Quaternion();
+				var e=new THREE.Euler();
+				var matrix = new THREE.Matrix4();
+
 				sphere.quaternion.onChange(function(){
-					if(enableFire){
-					ap.skinnedMesh.skeleton.bones[cbone].quaternion.copy( sphere.quaternion);
+					console.log("sphere.quaternion.onChange");
+					var tmp1=new THREE.Euler().setFromQuaternion(ap.skinnedMesh.skeleton.bones[cbone].quaternion);
+					AppUtils.printDeg(tmp1,"bone-q");
+					
+					//var tmp2=new THREE.Euler().setFromQuaternion(lastQ);
+					//AppUtils.printDeg(tmp2,"lastQ");
+					
+					
+					var tmp3=new THREE.Euler().setFromQuaternion(sphere.quaternion);
+					AppUtils.printDeg(tmp3,"sphere-q");
+					
+					//var inverse=q.copy(ap.skinnedMesh.skeleton.bones[cbone].quaternion).inverse();
+					//var inverse=q.copy(lastQ).inverse();
+					//var tmp4=new THREE.Euler().setFromQuaternion(inverse);
+					//AppUtils.printDeg(tmp4,"inverse");
+					
+					//var diff=inverse.multiply(sphere.quaternion);
+					
+					//var tmp5=new THREE.Euler().setFromQuaternion(diff);
+					//AppUtils.printDeg(tmp5,"diff");
+					var euler=e.setFromQuaternion(sphere.quaternion);
+					//var euler=e.setFromQuaternion(diff);
+					//console.log(euler.x,euler.y,euler.z);
+					//var r=ap.skinnedMesh.skeleton.bones[cbone].rotation;
+					var r=lastEuler;
+					var max=Math.abs(euler.x);
+					if(euler.y>max){
+						max=Math.abs(euler.y);
+					}
+					if(euler.z>max){
+						max=Math.abs(euler.z);
+					}
+					
+					//AppUtils.printDeg(euler,"diff");
+					if(max>THREE.Math.degToRad(180)){
+						AppUtils.printDeg(ap.skinnedMesh.skeleton.bones[cbone].rotation,"bone-r");
+						var tmp=new THREE.Euler().setFromQuaternion(sphere.quaternion);
+						AppUtils.printDeg(tmp,"sphere-r");
+						
+						console.log("ignore over move",max,euler.x,euler.y,euler.z);
+						sphere.enableFire=false;
+						var rotation=ap.skinnedMesh.skeleton.bones[cbone].rotation;//copy back
+						//sphere.rotation.copy(rotation);
+						//lastQ.copy(sphere.quaternion);
+						sphere.enableFire=true;
+						return;
+					}else{
+						AppUtils.printDeg(euler,"added");
+						euler.set(euler.x+r.x,euler.y+r.y,euler.z+r.z);
+						//enableFire=false;
+						var rotation=ap.skinnedMesh.skeleton.bones[cbone].rotation;
+						rotation.copy(euler);
+						ap.signals.boneRotationChanged.dispatch(cbone);
+						//sphere.rotation.copy(euler);//update local-q & bone-rotate&q;
+					}
+					
+					//enableFire=true;
+				});
+				/*sphere.rotation.onChange(function(){
+					console.log("sphere.rotation.onChange");
+					matrix.makeRotationFromEuler( sphere.rotation );
+					sphere.quaternion.setFromEuler( sphere.rotation, false );
+					if(sphere.enableFire){
+						ap.skinnedMesh.skeleton.bones[cbone].rotation.setFromRotationMatrix( matrix, undefined, false );
+						ap.skinnedMesh.skeleton.bones[cbone].quaternion.setFromRotationMatrix( matrix, undefined, false );
+					}else{
+						console.log("not fire");
 					}
 					ap.signals.boneRotationChanged.dispatch(cbone);
-				});
-				ap.skinnedMesh.skeleton.bones[cbone].quaternion.onChange(function(){
-					var quaternion=ap.skinnedMesh.skeleton.bones[cbone].quaternion;
-					ap.skinnedMesh.skeleton.bones[cbone].rotation.setFromQuaternion( quaternion, undefined, false );
-					enableFire=false;
-					sphere.quaternion.copy(quaternion);
-					enableFire=true;
 					
-				});
+					AppUtils.printDeg(ap.skinnedMesh.skeleton.bones[cbone].rotation,"final-bone-r");
+					AppUtils.printDeg(sphere.rotation,"final-sphere-r");
+				});*/
+				/*ap.skinnedMesh.skeleton.bones[cbone].rotation.onChange(function(){
+					console.log("bone.rotation.onChange");
+					var rotation=ap.skinnedMesh.skeleton.bones[cbone].rotation;
+					ap.skinnedMesh.skeleton.bones[cbone].quaternion.setFromEuler( rotation, undefined, false );
+					sphere.enableFire=false;
+					sphere.rotation.copy(rotation);
+					sphere.enableFire=true;
+					
+					
+					
+				});*/
 				ap.objects.push(sphere);
 			}
 			index++;
@@ -105,7 +186,7 @@ Example=function(application){
 		
 
 		
-		
+		this.boneIndex;
 		
 		ap.signals.transformSelectionChanged.add(function(target){
 			if(target==null){
@@ -114,8 +195,35 @@ Example=function(application){
 				ap.transformControls.setMode( "rotate" );
 				ap.transformControls.attach(target);
 				var boneIndex=target.boneIndex;
+				scope.boneIndex=boneIndex;
 				ap.signals.boneSelectionChanged.dispatch(boneIndex);
+				
+				var bone=scope.boneAttachControler.boneList[boneIndex];
+				var rotC=rotationControls[bone.name];
+				
+				rotC.enableFire=false;
+				rotC.rotation.set(0,0,0);
+				//rotC.quaternion.setFromEuler( bone.rotation, false );
+				rotC.enableFire=true;
+				//lastQ.copy(rotC.quaternion);
+				lastEuler.copy(bone.rotation);
+				
+				AppUtils.printDeg(rotC.rotation,"attached");
 			}
+		});
+		
+		ap.transformControls.addEventListener( 'mouseUp', function () {
+			var bone=scope.boneAttachControler.boneList[scope.boneIndex];
+			lastEuler.copy(bone.rotation);
+			var rotC=rotationControls[bone.name];
+			rotC.rotation.set(0,0,0);
+		});
+
+		ap.transformControls.addEventListener( 'mouseDown', function () {
+			var bone=scope.boneAttachControler.boneList[scope.boneIndex];
+			lastEuler.copy(bone.rotation);
+			var rotC=rotationControls[bone.name];
+			rotC.rotation.set(0,0,0);
 		});
 		
 
