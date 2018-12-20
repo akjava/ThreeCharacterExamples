83a7ad8aff07df3ca96dbdaec0eeb7e60b2c2186
Aki Miyazaki <akimisaki3@gmail.com>
2018-12-20 09:16:44 +0900

    refactoring

---
+++ WebContent/examples/ik/limitbone/js/Sidebar.IkReset.js
@@ -0,0 +1,27 @@
+Sidebar.IkReset=function(ap){
+	var resetPanel=new UI.TitlePanel("Reset Pose");
+	
+	
+	var buttonRow=new UI.ButtonRow("Reset All",function(){
+		AnimeUtils.resetPose(ap.skinnedMesh);
+		ap.signals.poseChanged.dispatch();
+	});
+	resetPanel.add(buttonRow);
+	var resetSelection=new UI.Button("Reset Selection");
+	resetSelection.onClick(function(){
+		BoneUtils.resetBone(ap.skinnedMesh,ap.boneSelectedIndex);
+	});
+	buttonRow.add(resetSelection);
+	
+	var resetIks=new UI.Button("Reset Iks");
+	resetIks.onClick(function(){
+		if(ap.ikControler.ikTarget!=null){
+			ap.ikControler.ikIndices.forEach(function(index){
+				BoneUtils.resetBone(ap.skinnedMesh,index);
+			});
+		}
+		
+	});
+	buttonRow.add(resetIks);
+	return resetPanel;
+}
