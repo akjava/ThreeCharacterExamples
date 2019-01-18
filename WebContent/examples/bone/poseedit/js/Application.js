380004bda64e2f0c6b47d766c5d5b65d85ab5dd9
Aki Miyazaki <akimisaki3@gmail.com>
2019-01-18 17:52:17 +0900

    support endsite

--- WebContent/examples/bone/poseedit/js/Application.js
+++ WebContent/examples/bone/poseedit/js/Application.js
@@ -22,6 +22,7 @@ var Application = function () {
 	var Signal = signals.Signal;
 
 	this.signals = {
+			ikSelectionChanged:new Signal(),
 			loadingTextureStarted:new Signal(),
 			loadingTextureFinished:new Signal(),
 			ikInitialized:new Signal(),
