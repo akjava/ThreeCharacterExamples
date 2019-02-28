a951a43a894447d9feba9088d1a495d12ca2f798
Aki Miyazaki <akimisaki3@gmail.com>
2019-02-28 20:58:22 +0900

    add preset

--- WebContent/js/sidebar/Sidebar.IkPresetIO.js
+++ WebContent/js/sidebar/Sidebar.IkPresetIO.js
@@ -15,7 +15,7 @@ Sidebar.IkPresetIO=function(ap,enableExport){
 	
 	
 	var jsonList=new ListLoadJsonDiv("../../../dataset/mbl3d/ikpreset/",
-			["","simple.json","front.json"],function(json){
+			["","simple.json","touch.json","front.json"],function(json){
 		
 		if(json==null){
 			ap.ikControler.setPresets(new IkPresets(ap.ikControler));
