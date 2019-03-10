00479499a36e47465ae21979fd09adfdcbbdbf8f
Aki Miyazaki <akimisaki3@gmail.com>
2019-03-10 17:53:12 +0900

    refactoring

--- WebContent/js/core/BasicCore.js
+++ WebContent/js/core/BasicCore.js
@@ -1,71 +1,14 @@
-var BasicCore = function ( application ) {
-	var ap=application;
-	var scope=this;
-	var signals = application.signals;
-	
-	var container = new UI.Panel();
-	container.setId( 'viewport' );
-	ap.core=container;
-	
-	
-	
-	var camera = application.camera;
-	var scene = application.scene;
-	
-	//init renderer
-	var renderer = new THREE.WebGLRenderer( { antialias: true } );
-	renderer.shadowMap.enabled = true;
-	renderer.shadowMap.type = THREE.PCFSoftShadowMap; //shadow type is fixed so far.
-	renderer.setPixelRatio( window.devicePixelRatio );
-	renderer.setSize(0,0 );//windowResize call from html
-	container.dom.appendChild( renderer.domElement );
-	application.renderer=renderer;
-	
-	//control
-	var controls = new THREE.OrbitControls( camera ,renderer.domElement);
-	controls.screenSpacePanning=true;
-	application.controls=controls;
-	
-	//camera & control position will set in Examples
-	
-	signals.windowResize.add( function () {
-		camera.aspect = container.dom.offsetWidth / container.dom.offsetHeight;
-		camera.updateProjectionMatrix();
-		
-		renderer.setSize( container.dom.offsetWidth, container.dom.offsetHeight );
-	} );
-	
-	function animate() {
-		requestAnimationFrame( animate );
-		ap.onRender();
-	}
-	
-	function render() {
-		renderer.render( scene, camera );
-		ap.signals.rendered.dispatch();
-	}
-	
-	ap.onRender=render;
-	
-	ap.boneSelectedIndex=0;
-	ap.getSignal("boneSelectionChanged").add(function(index){
-		ap.boneSelectedIndex=index;
-	});
-	var root=new THREE.Group();
-	ap.root=root;
-	ap.scene.add(root);
-	
-	ap.convertToZeroRotatedBoneMesh=true;
-	ap.convertBoneEulerOrders=true;
+var BasicCore = function ( ap ) {
+	MinCore.call(this,ap);
 	
 	ap.signals.loadingModelStarted.add(function(url,fileName){
 		if(ap.skinnedMesh!=null && ap.skinnedMesh.parent!=null){
 			ap.skinnedMesh.parent.remove(ap.skinnedMesh);
 		}
 		if(fileName){
-			application.modelFileName=fileName;
+			ap.modelFileName=fileName;
 		}else{
-			application.modelFileName=url;
+			ap.modelFileName=url;
 		}
 		AppUtils.loadMesh(url,function(mesh){
 			try{
@@ -94,7 +37,5 @@ var BasicCore = function ( application ) {
 		},fileName);
 	});
 	
-	animate();
-	
-	return container;
+	return ap.core;
 }
